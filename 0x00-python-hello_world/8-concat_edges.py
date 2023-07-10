05f7f53c27947e7019393bf19427980d9b50f595
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-07-10 14:16:59 -0700

    Task 8

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str1 = str[39:66]
+str2 = " " + "with python"
+str = str1 + str2
+print(str)
