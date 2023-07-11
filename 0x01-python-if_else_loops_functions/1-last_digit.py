0c02ac29355c78dfce3ec4daf230c2837f0ee1a8
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-07-11 04:11:43 -0700

    test task 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+str1 = "Last digit of" + "{number:d}"
+str2 = "is" + " " + "{number[-1]" + " "
+
+if number[-1] > 5:
+    print("str1 + str2 + and is greater than 5")
+elif number[-1] == 0:
+    print("str1 + str2 + and is 0")
+elif number[-1] < 6 and != 0:
+    print("str1 + str2 + and is less than 6 and not 0")
