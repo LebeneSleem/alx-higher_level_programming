bde3fb4889f361468a80ac61614a7521263e9272
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-07-17 16:38:58 -0700

    TAsk10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,8 +5,8 @@ def divisible_by_2(my_list=[]):
     multiples = []
     for each_number in my_list:
         if each_number % 2 == 0:
-            result.append(True)
+            multiples.append(True)
         else:
-            result.append(False)
+            multiples.append(False)
 
             return (result)
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-     
     if len(my_list) == 0:
         return (None)
-
     else:
         biggest_int = my_list[0]
-
         for a in range(len(my_list)):
-        if my_list[a] > biggest_int:
-            biggest_int = my_list[a]
+            if my_list[a] > biggest_int:
+                biggest_int = my_list[a]
 
-    return (biggest_int)
+                return (biggest_int)
