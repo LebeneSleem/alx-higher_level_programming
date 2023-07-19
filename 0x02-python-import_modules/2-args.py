50dd98b82fe3b2ac8b20335fcd4107c98b488009
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-07-19 10:44:38 -0700

    Task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-
+    """Print the number of and list of arguments."""
     import sys
 
     count = len(sys.argv) - 1
-    if count = 0:
+    if count == 0:
         print("0 arguments.")
     elif count == 1:
         print("1 argument:")
@@ -13,5 +13,3 @@ if __name__ == "__main__":
         print("{} arguments:".format(count))
     for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
-
-    print()
