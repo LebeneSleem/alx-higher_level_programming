252d5bc74143160fa6c3e68efe1ebbfb17a282ab
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-08-01 08:33:15 -0700

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 1-square.py by Lebene Gbebleou-Sleem
+
+"""creating a class to defines a Square by 0-squre.py"""
+
+
+class Square:
+
+    """creating a private instance attribute"""
+    def __init__(self):
+        self.__size = ""
