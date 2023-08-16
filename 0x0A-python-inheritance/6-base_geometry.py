d703339bc8aedf0d0cd52fd846a3ed423935a002
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-08-16 06:29:36 -0700

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """This is an empty class"""
+
+    def area(self):
+        """method which is not implemented"""
+        raise Exception("area() is not implemented")
