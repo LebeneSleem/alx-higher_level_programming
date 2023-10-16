177814187e60400860c24923fb6391c5e7dff271
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-10-16 15:29:47 -0700

    script that prints two arguments passed to it, in the following format: “ is ”

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+function printArguments () {
+  if (process.argv[2]) {
+    if (process.argv[3]) {
+      console.log(process.argv[2] + ' is ' + process.argv[3]);
+    } else {
+      console.log(process.argv[2] + ' is undefined');
+    }
+  } else {
+    console.log('undefined is undefined');
+  }
+}
+
+printArguments();
