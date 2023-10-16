6a7a2dd9fec475c3127cc2b2622540dbe313f815
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-10-16 14:43:25 -0700

    script that prints a message depending of the number of arguments passed:

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+function PrintArguments () {
+  const Arguments = process.argv.length - 2;
+
+  if (Arguments === 0) {
+    console.log('No argument');
+  } else if (Arguments === 1) {
+    console.log('Argument found');
+  } else {
+    console.log('Arguments found');
+  }
+}
+PrintArguments();
