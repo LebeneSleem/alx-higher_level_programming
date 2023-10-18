a1ff289b900218218e6f2e2c4f551071c2cb69c7
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-10-18 11:04:14 -0700

    error in code fixed

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,8 +4,15 @@ const MainSquare = require('./5-square');
 
 class Square extends MainSquare {
   charPrint (c = 'X') {
-    for (let i = 0; i < this.size; i++) {
-      console.log(c.repeat(this.size));
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let space = '';
+      for (let j = 0; j < this.width; j++) {
+        space += c;
+      }
+      console.log(space);
     }
   }
 }
