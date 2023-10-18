54db2ad6266448c7b92904eb03ce224950855b69
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-10-18 12:01:41 -0700

    a script that concats 2 files.

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,14 +5,17 @@ const sourceFilePath1 = process.argv[2];
 const sourceFilePath2 = process.argv[3];
 const destinationFilePath = process.argv[4];
 
-function concatenateFiles() {
-    const fileContents1 = fs.readFileSync(sourceFilePath1, 'utf8');
-    const fileContents2 = fs.readFileSync(sourceFilePath2, 'utf8');
-    const concatenatedContents = fileContents1 + fileContents2;
+function concatenateFiles () {
+  const fileContents1 = fs.readFileSync(sourceFilePath1, 'utf8');
+  const fileContents2 = fs.readFileSync(sourceFilePath2, 'utf8');
+  const concatenatedContents = fileContents1 + fileContents2;
 
-    fs.writeFileSync(destinationFilePath, concatenatedContents);
+  fs.writeFileSync(destinationFilePath, concatenatedContents);
 }
 
 if (sourceFilePath1 && sourceFilePath2 && destinationFilePath) {
-    concatenateFiles();
+  concatenateFiles();
+  console.log(`Files concatenated and saved to ${destinationFilePath}`);
+} else {
+  console.error('Usage: node concat.js <sourceFilePath1> <sourceFilePath2> <destinationFilePath>');
 }
