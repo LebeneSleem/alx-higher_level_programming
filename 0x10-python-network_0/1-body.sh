5b9bd89705e341cb56573a2370ca85403877a0de
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-11-02 06:26:47 -0700

    Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-
-response=$(curl -s -w "%{http_code}" "$1")
-if [[ $response == 200 ]]; then
-  body=$(curl -s "$1")
-  echo "$body"
-fi
+curl -s -w "%{http_code}" "$1" | { read code; [ $code == 200 ] && cat; }
