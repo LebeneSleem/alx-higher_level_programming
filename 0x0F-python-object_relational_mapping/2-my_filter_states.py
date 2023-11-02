c3253a1ac1e8668fd7b3eed023f161974dc6fabb
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-11-02 09:58:24 -0700

    error fixed

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,20 +4,35 @@ This script displays all values in the states table of
 hbtn_0e_0_usa where the name matches the provided argument.
 """
 
-#!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
+if __name__ == '__main__':
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database_name> <state_name>".format(sys.argv[0]))
+        sys.exit(1)
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
-    rows = cur.fetchall()
-    for row in rows:
+    username, password, database_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+
+    db_connection = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database_name,
+        port=3306
+    )
+
+    db_cursor = db_connection.cursor()
+
+    # Define the SQL query with string formatting
+    sql_query = "SELECT * FROM states WHERE name = %s ORDER BY states.id"
+
+    db_cursor.execute(sql_query, (state_name,))
+
+    rows_selected = db_cursor.fetchall()
+
+    for row in rows_selected:
         print(row)
-    cur.close()
-    db.close()
+
+    db_cursor.close()
+    db_connection.close()
