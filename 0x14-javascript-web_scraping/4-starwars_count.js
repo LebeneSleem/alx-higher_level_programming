355f938fd130e318cb0317508ebe30dcc6aa93d6
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2023-11-28 07:37:17 -0800

    script that prints the number of movies where the character “Wedge Antilles” is present.

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,26 +1,22 @@
 #!/usr/bin/node
 
 const request = require('request');
+const url = process.argv[2];
+const characterId = '18';
+let count = 0;
 
-if (process.argv.length !== 3) {
-  console.error('Usage: ./4-starwars_count.js <API-URL>');
-  process.exit(1);
-}
-
-const apiUrl = process.argv[2];
-
-request(apiUrl, (error, response, body) => {
+request.get(url, (error, response, body) => {
   if (error) {
-    console.error(error);
-    process.exit(1);
-  }
-
-  if (response.statusCode === 200) {
-    const films = JSON.parse(body).results;
-    const wedgeAntillesMovies = films.filter((film) => film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/'));
-    console.log(wedgeAntillesMovies.length);
+    console.log(error);
   } else {
-    console.error(`Failed to retrieve information. Status code: ${response.statusCode}`);
-    process.exit(1);
+    const data = JSON.parse(body);
+    data.results.forEach((film) => {
+      film.characters.forEach((character) => {
+        if (character.includes(characterId)) {
+          count += 1;
+        }
+      });
+    });
+    console.log(count);
   }
 });
